d5c5262a96e59d3a720e512b7ab57a0ec867e2d6
Artem Yanchenko <artem.yanchenko.94@mail.ru>
2023-03-09 15:56:17 +0300

    final edit microtask Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
-import FullInput from './components/FullInput';
+import Input from './components/Input';
+import Button from './components/Button';
 
 
 function App() {
@@ -10,14 +11,23 @@ function App() {
         {messageTitle: 'message3'}
     ])
 
-    const addMessage = (newTitleMessage: string) => {
-        let newMessage = {messageTitle: newTitleMessage};
+    let [title, setTitle] = useState('')
+
+    const addMessage = (title:string) => {
+        let newMessage = {messageTitle: title};
         setMessage([newMessage, ...message]);
+        setTitle('');
+    }
+
+    const onClickButtonHandler = () => {
+        addMessage(title);
     }
 
     return (
         <div className={'App'}>
-            <FullInput addMessage={addMessage}/>
+            <Input title={title} setTitle={setTitle}/>
+            <Button name={'add'} callBack={onClickButtonHandler}/>
+
             {message.map((m, index) =>
                 <div key={index}>{m.messageTitle}</div>)}
         </div>
